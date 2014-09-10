380d0549d3363829fe78df729ad1236f20c579fb
shokokb <shokokb@kobayashishouko-no-MacBook-Pro.local>
2014-09-10 18:20:49 +0900

    HTML→HTMLを汎用ルーチンに変更した。

--- js/NoteItem.js
+++ js/NoteItem.js
@@ -1,57 +1,71 @@
-///@summary コンストラクタ
-function NoteItem(i_text, i_bgcolor) {
+// ///@summary コンストラクタ
+// function NoteItem($i_jquery) {
+//   $jquery = $i_jquery;
+// }
 
-  ///@param クラス名
-  this.name = 'NoteItem';
+///@summary コンストラクタ
+function NoteItem() {
 
   // @param 付箋のID(MAX値)
   const MAX = 9999999999;
   // @param 付箋のID
-  this.id = 'ID' + Math.round(Math.random() * MAX);
+  var id = /*'ID' +*/ Math.round(Math.random() * MAX);
 
   // 付箋を生成する
   $jquery = $(
     '<div ' + 
-    'id="' + this.id + '" ' + 
-    'class="' + this.name + '" ' + 
+    'id="' + id + '" ' + 
+    'class="' + 'NoteItem' + '" ' + 
     '>' + 
     <!--削除ボタン-->
-    '<button id="del" class="btn btn-danger btn-xs">x</button>' + 
+    '<button id="del" class="btn btn-default btn-xs">x</button>' + 
     <!--最小化ボタン-->
-    '<button id="min" class="btn btn-info btn-xs" >_</button>' + 
+    '<button id="min" class="btn btn-default btn-xs" >_</button>' + 
+    <!--画像添付ボタン-->
+    '<button id="attachImg" class="btn btn-default btn-xs">画像</button>' + 
     <!--タグ表示用-->
     '<div id="tags"></div>' +
     <!--入力フォーム-->
     '<div id="form"></div>' +
+    <!--イメージ添付用-->
+    '<div id="img"></div>' +
     <!--文字列入力用--> 
-    '<div id="remarks">' + i_text + '</div>' + 
+    '<div id="remarks" name="remarks">&nbsp;</div>' + 
     '</div>'
   );
-
-  // 「最小化」ボタンを押下したときには、文字列を表示／非表示を設定する。
-  $jquery.find('#min').click(function(){
-    if ($(this).parent().find('#remarks').css('display') == 'block') {
-      // 表示されている場合は、文字列を非表示とする。
-      $(this).parent().find('#tags').show();
-      $(this).parent().find('#remarks').hide();
-    } else {
-      // 非表示の場合は、文字列を表示する。
-      $(this).parent().find('#tags').hide();
-      $(this).parent().find('#remarks').show();
-    }
-  });
-  //　「削除」ボタンが押されたときには、付箋を削除する。
-  $jquery.find('#del').click(function(){
-    $(this).parent().remove();
-  });
+  // 表示されている場合は、文字列を非表示とする。
+  $(this).parent().find('[name=tags]').show();
 
   // 付箋をリサイズ・ドラッグ可能とする。
   //$jquery.resizable({handles : 's'});
   //$jquery.draggable();
+
+  // @summary 「最小化」ボタンを押下したときには、文字列を表示／非表示を設定する。
+  $jquery.find('#min').click(function()
+  {
+    if ($(this).parent().find('[name=remarks]').css('display') == 'block') 
+    {
+      // 表示されている場合は、文字列を非表示とする。
+      $(this).parent().find('[name=tags]').show();
+      $(this).parent().find('[name=remarks]').hide();
+    } 
+    else 
+    {
+      // 非表示の場合は、文字列を表示する。
+      $(this).parent().find('[name=tags]').hide();
+      $(this).parent().find('[name=remarks]').show();
+    }
+  });
+
+  //　@summary 「削除」ボタンが押されたときには、付箋を削除する。
+  $jquery.find('#del').click(function(){
+    $(this).parent().remove();
+  });
+
   
   // 付箋をダブルクリック時に入力欄を表示する。
   $jquery.dblclick(function(){
-    var memo = $(this).find('#remarks').html();
+    var memo = $(this).find('[name=remarks]').html();
     area.instanceById('area1').setContent(memo);
     $('input#selectedNoteItem').val($(this).attr('id'));
   });
@@ -64,12 +78,6 @@ function NoteItem(i_text, i_bgcolor) {
     $jquery.appendTo(i_to);  
     $jquery.dblclick();
   }
-
-  ///@summary クラス名を取得する。
-  this.getName = function() 
-  {
-    return this.name;
-  };
 }
 
 // @summary 文字列を変更する。
@@ -86,82 +94,73 @@ NoteItem.ChangeVal = function($jquery, i_memo)
     //console.log($($strongs[index]).text());
   }
   
-  $jquery.find('#tags').html(tags);
-  $jquery.find('#remarks').html(i_memo);
+  $jquery.find('[name=tags]').html(tags);
+  $jquery.find('[name=remarks]').html(i_memo);
 }
 
-/// @summary 公式メソッド
-NoteItem.prototype.publicMethod = function() {
-  console.log("NoteItem's publicMethod");
-};
+// /// @summary 公式メソッド
+// NoteItem.prototype.publicMethod = function() {
+//   console.log("NoteItem's publicMethod");
+// };
 
 ///@summary 病名コンストラクタ
-function NoteItemDisease(i_text) 
+function NoteItemDisease() 
 {
-  NoteItem.call(
-    this, 
-    i_text//,                       // 入力文字列
-  );
-
-  this.name = 'NoteItemDisease';
+  NoteItem.call(this/*, i_text*/);  // 入力文字列
 
   //--JQuery オブジェクト操作---//
   // クラス属性を追加した。
-  $jquery.addClass(this.name);
+  $jquery.attr('name', 'NoteItemDisease');
+  $jquery.addClass('NoteItemDisease');
   //console.log($jquery);
   //--JQuery オブジェクト操作---//
 }
 NoteItemDisease.prototype = new NoteItem;
 
 ///@summary 主訴コンストラクタ
-function NoteItemComplaint(i_text) 
+function NoteItemComplaint() 
 {
-  NoteItem.call(
-      this, 
-      i_text//,                         // 入力文字列
-  );
+  NoteItem.call(this/*, i_text*/);  // 入力文字列
 
-  this.name = 'NoteItemComplaint';
   //--JQuery オブジェクト操作---//
   // クラス属性を追加した。
-  $jquery.addClass(this.name);
+  $jquery.attr('name', 'NoteItemComplaint');
+  $jquery.addClass('NoteItemComplaint');
   //console.log($jquery);
   //--JQuery オブジェクト操作---//
 }
 NoteItemComplaint.prototype = new NoteItem;
 
 ///@summary 検査コンストラクタ
-function NoteItemMedicalCheck(i_text) 
+function NoteItemMedicalCheck() 
 {
-  NoteItem.call(
-      this, 
-      i_text//,                         // 入力文字列
-  );
-
-  this.name = 'NoteItemMedicalCheck';
+  NoteItem.call(this/*, i_text*/);  // 入力文字列
 
   //--JQuery オブジェクト操作---//
   // クラス属性を追加した。
-  $jquery.addClass(this.name);
-  $jquery.find('#form').append('<div>視力検査</div><input type=text value="test"/>');
+  $jquery.attr('name', 'NoteItemMedicalCheck');
+  $jquery.addClass('NoteItemMedicalCheck');
+  //$jquery.find('#form').append('<div>視力検査</div><input type=text value="test"/>');
   //console.log($jquery);
   //--JQuery オブジェクト操作---//
 }
 NoteItemMedicalCheck.prototype = new NoteItem;
 
 ///@summary 処方コンストラクタ
-function NoteItemPrescription(i_text) 
+function NoteItemPrescription() 
 {
-  NoteItem.call(
-      this, 
-      i_text//,                         // 入力文字列
-  );
-
-  this.name = 'NoteItemPrescription';
+  NoteItem.call(this/*, i_text*/);  // 入力文字列
 
   //--JQuery オブジェクト操作---//
   // クラス属性を追加した。
-  $jquery.addClass(this.name);
+  $jquery.attr('name', 'NoteItemPrescription');
+  $jquery.addClass('NoteItemPrescription');
+  $jquery.find('#form').attr('name', 'medicine');
+  $jquery.find('#form').append(
+    '<input name="medicine-orca" type="hidden" value="ORCAID" />' + 
+    '<input name="medicine-name" type="disable" value="タリビッド眼軟膏0.3%"/>' + 
+    '<input name="medicine-cnt"  type="text" value="1">個'
+  );
   //console.log($jquery);
   //--JQuery オブジェクト操作---//
 }
@@ -169,36 +168,28 @@ NoteItemPrescription.prototype = new NoteItem;
 
 
 ///@summary 手術コンストラクタ
-function NoteItemOperation(i_text) 
+function NoteItemOperation() 
 {
-  NoteItem.call(
-      this, 
-      i_text//,                         // 入力文字列
-  );
-
-  this.name = 'NoteItemOperation'; 
+  NoteItem.call(this/*, i_text*/);  // 入力文字列
 
   //--JQuery オブジェクト操作---//
   // クラス属性を追加した。
-  $jquery.addClass(this.name);
+   $jquery.attr('name', 'NoteItemOperation');
+ $jquery.addClass('NoteItemOperation');
   //console.log($jquery);
   //--JQuery オブジェクト操作---//
 }
 NoteItemOperation.prototype = new NoteItem;
 
 ///@summary メモコンストラクタ
-function NoteItemMemo(i_text) 
+function NoteItemMemo() 
 {
-  NoteItem.call(
-      this, 
-      i_text//,                         // 入力文字列
-  );
-
-  this.name = 'NoteItemMemo';
+  NoteItem.call(this/*, i_text*/);  // 入力文字列
 
   //--JQuery オブジェクト操作---//
   // クラス属性を追加した。
-  $jquery.addClass(this.name);
+  $jquery.attr('name', 'NoteItemMemo');
+  $jquery.addClass('NoteItemMemo');
   //console.log($jquery);
   //--JQuery オブジェクト操作---//
 
--- js/sample.js
+++ js/sample.js
@@ -21,7 +21,18 @@ $(function()
 	// 「保存」ボタンを押下時、XMLを保存する。
 	$('button#save').click(function(){
 		//alert($('td#note').children().html());
-		console.log($('td#note').children());
+		//console.log($('td#note').children());
+		//var xml = Utility.HtmlInputItemToXml($('td#note').children());
+		//console.log(xml);
+		// （デモ）処方箋をXMLに変更する。
+		var xml = '';
+		$('#NoteItemContainerPrescription > .NoteItem').each(function(){
+			var xml = Utility.HtmlInputItemToXml($(this));
+		})
+	    console.log('Utility.HtmlInputItemToXml : ' + xml);
+
+		//Utility.HtmlInputItemToXml($('<input name="tag" type="text" value="testtext"/>'));
+
 	});
 
 	// 「患者情報」ボタンを押下時、患者情報を表示する。
@@ -56,7 +67,6 @@ $(function()
 
 	// ■初回起動時、主訴欄を生成する。
 	//new NoteItemComplaint('本日の主訴を入力してください。');
-	Utility.HtmlToXml($('<input name="tag" type="text" value="testtext"/>'));
 
 });
 
@@ -65,38 +75,63 @@ function Utility() {
 }
 
 /// @summary 	HTMLをXMLに保存する。
-/// @param 		$i_jquery HTML（入力フォーム）例:input,textarea,select ...等
-Utility.HtmlToXml = function($i_jquery)
+/// @param 		$i_jquery HTML（入力フォーム）を含む例:input,textarea,select ...等
+Utility.HtmlInputItemToXml = function($i_jquery)
 {
 	var retVal = '';
+	//const childElementTags = {'DIV', 'INPUT', 'textarea', 'SELECT'};
+
+	//console.log($i_jquery);
 
 	// name属性をタグとして使用する。
 	var tag = $i_jquery.attr('name');
-	// 内部文字列（数値など）を登録する。
-	var innerText = '';
 
 	// XMLを生成する。
-	if(tag != '')
+	if(tag !== undefined)
 	{
+		retVal += '<' + tag + '>';
+	
+		// 子供を持っている場合は再起的にXMLを生成する。
+		var hasChildren = $i_jquery.children(
+			'DIV', 
+			'INPUT', 
+			'TEXTAREA', 
+			'SELECT'
+		).length > 0;
+
+		// 内部文字列を取得する
 		switch ($i_jquery[0].tagName)
 		{
 			case 'DIV' :  // DIVタグの場合
-				innerText = $i_jquery.html();
-			case 'INPUT' : // INPUTタグの場合
-				innerText = $i_jquery.attr('value');
+				console.log($i_jquery.children().length);
+				if(hasChildren == false) retVal += $i_jquery.html();
 				break;
+			case 'INPUT' : // INPUTタグの場合
+				retVal += $i_jquery.attr('value');
+				break;
+			// TODO : TEXTAREA
 			// TODO : SELECTボックスなど
 			default : 
+				console.log($i_jquery[0].tagName);
 				// それ以外の場合は出力しない。
 				break;		
 		}
 
-		retVal += '<' + tag + '>';
-		retVal += innerText;
+		// 子供の要素を再起的に呼び出す。
+		$i_jquery.children().each(function () {
+			retVal += Utility.HtmlInputItemToXml($(this));
+		})
+
 		retVal += '</' + tag + '>';		
 	}
 
-	console.log('Utility.HtmlToXml : ' + retVal);
+	if (tag == 'medicine') 
+	{
+		alert(retVal + '/' + $i_jquery.find().length);
+		console.log($i_jquery);
+	}
+
+	console.log(retVal);
 	return retVal;
 }
 
