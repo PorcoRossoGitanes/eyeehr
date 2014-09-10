42d33c672f7fabe0580fd7ce440cf70d0efc90fb
shokokb <shokokb@kobayashishouko-no-MacBook-Pro.local>
2014-09-10 11:11:51 +0900

    HTML(JQuery オブジェクト）をXMLに変換する関数を追加した。

--- js/sample.js
+++ js/sample.js
@@ -56,6 +56,47 @@ $(function()
 
 	// ■初回起動時、主訴欄を生成する。
 	//new NoteItemComplaint('本日の主訴を入力してください。');
+	Utility.HtmlToXml($('<input name="tag" type="text" value="testtext"/>'));
+
 });
 
+///@summary コンストラクタ
+function Utility() {
+}
+
+/// @summary 	HTMLをXMLに保存する。
+/// @param 		$i_jquery HTML（入力フォーム）例:input,textarea,select ...等
+Utility.HtmlToXml = function($i_jquery)
+{
+	var retVal = '';
+
+	// name属性をタグとして使用する。
+	var tag = $i_jquery.attr('name');
+	// 内部文字列（数値など）を登録する。
+	var innerText = '';
+
+	// XMLを生成する。
+	if(tag != '')
+	{
+		switch ($i_jquery[0].tagName)
+		{
+			case 'DIV' :  // DIVタグの場合
+				innerText = $i_jquery.html();
+			case 'INPUT' : // INPUTタグの場合
+				innerText = $i_jquery.attr('value');
+				break;
+			// TODO : SELECTボックスなど
+			default : 
+				// それ以外の場合は出力しない。
+				break;		
+		}
+
+		retVal += '<' + tag + '>';
+		retVal += innerText;
+		retVal += '</' + tag + '>';		
+	}
+
+	console.log('Utility.HtmlToXml : ' + retVal);
+	return retVal;
+}
 
