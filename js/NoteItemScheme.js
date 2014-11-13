8992f44d6918c668acc12304294efd0f609f337c
shokokb <shokokb@kobayashishouko-no-MacBook-Pro.local>
2014-11-13 15:21:25 +0900

    （１）読込時、ファイル添付、備考、シェーマはNoteItemから継承した処理を行うように変更した。

--- js/NoteItemScheme.js
+++ js/NoteItemScheme.js
@@ -34,15 +34,7 @@ function NoteItemScheme()
     {
       if ($i_xml[0].tagName == $jquery.attr('name'))
       {
-        //console.log($i_xml);
-        // TODO : 定型フォーマット部分を追加する。
-        //$jquery.find('[name=formats]').html($i_xml.children('formats').html());
-        // 画像添付部分を追加する。
-        $jquery.find('[name=attachments]').html($i_xml.children('attachments').html());
-        // 備考部分を追加する。
-        $jquery.find('[name=schemes]').html($i_xml.children('schemes').html());
-        // 備考部分を追加する。
-        $jquery.find('[name=remarks]').html($i_xml.children('remarks').html());
+        _super.setByXml.call(this, $i_xml);
       }
     }
   }
