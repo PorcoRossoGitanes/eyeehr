998d7baa04d982e2d58b5faeeacfb9cf8c5b9bfa
shokokb <shokokb@kobayashishouko-no-MacBook-Pro.local>
2014-12-03 21:28:20 +0900

    （１）コメントを修正した。

--- js/Config/Config.js
+++ js/Config/Config.js
@@ -5,6 +5,10 @@
  */
 function Config() {}
 
+/**
+ * システム設定を読み込む。
+ * @return {Object} システム設定(JSONオブジェクト)
+ */
 Config.Load = function() {
     const URL = 'js/json/configure.json';
     var ret = Utility.LoadJson(URL);
