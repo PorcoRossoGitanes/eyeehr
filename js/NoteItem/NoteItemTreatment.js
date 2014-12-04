c4dad68f39456618a42264fb2eecedf08926db13
shokokb <shokokb@kobayashishouko-no-MacBook-Pro.local>
2014-12-04 10:16:54 +0900

    （１）NoteItemContainerTreatment, NoteItemTreatmentを追加した。

---
+++ js/NoteItem/NoteItemTreatment.js
@@ -0,0 +1,54 @@
+/**
+ * ノートアイテム　処置
+ * @class NoteItemTreatment
+ * @extends NoteItem
+ * @constructor
+ */
+function NoteItemTreatment() {
+    NoteItem.call(this);
+
+    /**
+     * @property {String} _name クラス名
+     */
+    this._name = 'NoteItemTreatment';
+
+    //--JQuery オブジェクト操作---//
+    // クラス属性を追加した。
+    $(this._jquery).attr('name', this._name);
+    $(this._jquery).addClass(this._name);
+    //--JQuery オブジェクト操作---//
+
+};
+(function() {
+
+    // 継承設定 
+    var Super = function Super() {};
+    Super.prototype = NoteItem.prototype;
+    NoteItemTreatment.prototype = new Super();
+    var _super = Super.prototype;
+    var _proto = NoteItemTreatment.prototype;
+
+    /** 
+     * XMLを設定する。
+     * @method setByXml
+     * @param {String} i_xml XML文字列
+     */
+    _proto.setByXml = function(i_xml) {
+        if (i_xml !== undefined) {
+            if ($(i_xml)[0].tagName == $(this._jquery).attr('name')) {
+                _super.setByXml.call(this, i_xml);
+            }
+        }
+    }
+
+    /**
+     * クラス名（親クラス...現在のクラス）を取得する
+     * @method getName
+     * @return {String} クラス名（親クラス...現在のクラス）
+     */
+    _proto.getName = function() {
+        var name = _super.getName.call(this);
+        return name + ' ' + this._name;
+    };
+
+})();
---
+++ js/NoteItemContainer/NoteItemContainerTreatment.js
@@ -0,0 +1,79 @@
+/**
+ * ノートアイテムコンテナー　処置
+ * @class NoteItemContainerTreatment
+ * @extends NoteItemContainer
+ * @constructor
+ */
+function NoteItemContainerTreatment() {
+    NoteItemContainer.call(this);
+
+    /**
+     * @property {String} _name クラス名
+     */
+    this._name = 'NoteItemContainerTreatment';
+
+    /**
+     * @property　{String} _title　タイトル
+     */
+    this._title = '処置';
+
+    /**
+     * @property {String} _left 左座標
+     * @example 'auto', '100px'
+     */
+    this._left = '800px';
+
+    /**
+     * @property {String} _top 上座標
+     * @example 'auto', '100px'
+     */
+    this._top = '100px';
+
+    //--JQuery オブジェクト操作---//
+    $(this._jquery).addClass(this._name);
+    $(this._jquery).attr('name', this._name);
+    $(this._jquery).find('#title').text(this._title);
+    //--JQuery オブジェクト操作---//
+
+    this.update();
+
+};
+(function() {
+
+    // 継承設定
+    var Super = function Super() {};
+    Super.prototype = NoteItemContainer.prototype;
+    NoteItemContainerTreatment.prototype = new Super();
+    var _super = Super.prototype;
+    var _proto = NoteItemContainerTreatment.prototype;
+
+    /** 
+     * クラス名（親クラス...現在のクラス）を取得する
+     * @method getName
+     * @return クラス名（親クラス...現在のクラス）
+     */
+    _proto.getName = function() {
+        var name = _super.getName.call(this);
+        return name + ' ' + this._name;
+    };
+
+    /**
+     * Xmlを設定する
+     * @method setByXml
+     * @param {JQuery Object} $i_xml XML</NoteItemContainerXXX />
+     */
+    _proto.setByXml = function($i_xml) {
+        _super.setByXml.call(this, $i_xml);
+
+        // 既存のXMLデーターが存在する場合は、データーをDOMに追加する。
+        if ($i_xml !== undefined) {
+            if ($i_xml[0].tagName == $(this._jquery).attr('name')) {
+                for (var index = 0; index < this._xml.children.length; index++) {
+                    var item = new NoteItemTreatment();
+                    item.setByXml($(this._xml.children[index]));
+                    $(this._jquery).append(item.getJQueryObject());
+                }
+            }
+        }
+    }
+})();
