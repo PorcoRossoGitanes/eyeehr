8255ebbc22dc6159f8526e00b6ecbce7a219b039
shokokb <shokokb@kobayashishouko-no-MacBook-Pro.local>
2014-12-05 13:37:10 +0900

    （１）test.php：新規クラスのJS読込コードを追加した。

--- test.php
+++ test.php
@@ -6,9 +6,7 @@
     <script src="js/jquery-2.1.1.js"></script><!-- jQuery 2.1.1 -->
     <script src="js/jquery-ui/jquery-ui.js"></script><!-- jQuery-ui 1.11.1 -->
     <link href="css/bootstrap/bootstrap.min.css" rel="stylesheet"><!-- Bootstrap -->
-    <!--自作CSS-->    
-    <link href="css/style.css" rel="stylesheet" type="text/css">
-    <!--自作CSS-->    
+    <link href="css/style.css" rel="stylesheet" type="text/css"><!--自作CSS-->    
   </head>
   <body>
     <h1>電子カルテ テスト</h1>
@@ -225,15 +223,18 @@ $('button#OrcaModifyMedicalInfo').click(function(){
       </table>
     </div>
 <!--自作JS ※ロードの都合上、本ファイルの末尾で登録する。-->
-  <script src="js/jquery.base64/jquery.base64.js"></script>
+    <script src="js/jquery.base64/jquery.base64.js"></script>
   
   <script src="js/Utility/Utility.js"></script>
+  
+  <script src="js/Config/Config.js"></script>
 
   <script src="js/Staff/Staff.js"></script>
   
   <script src="js/Patient/Patient.js"></script>
 
   <script src="js/Stamp/Stamp.js"></script>
+  <script src="js/Stamp/StampComplaint.js"></script>
   <script src="js/Stamp/StampDisease.js"></script>
   <script src="js/Stamp/StampInjection.js"></script>
   <script src="js/Stamp/StampMachine.js"></script>
@@ -241,12 +242,27 @@ $('button#OrcaModifyMedicalInfo').click(function(){
   <script src="js/Stamp/StampMedicalProduct.js"></script>
   <script src="js/Stamp/StampOperation.js"></script>
   <script src="js/Stamp/StampTreatment.js"></script>
+  <script src="js/Stamp/StampMemo.js"></script>
+
+  <script src="js/StampList/StampList.js"></script>
+  <script src="js/StampList/StampListComplaint.js"></script>
+  <script src="js/StampList/StampListDisease.js"></script>
+  <script src="js/StampList/StampListMedicalCheck.js"></script>
+  <script src="js/StampList/StampListTreatment.js"></script>
+  <script src="js/StampList/StampListMedicalProduct.js"></script>  
+  <script src="js/StampList/StampListOperation.js"></script>
+  <script src="js/StampList/StampListInjection.js"></script>
+  <script src="js/StampList/StampListMachine.js"></script>
+  <script src="js/StampList/StampListMemo.js"></script>
+
+  <script src="js/StampGadget/StampGadget.js"></script>
 
   <script src="js/NoteItem/NoteItem.js"></script>
   <script src="js/NoteItem/NoteItemComplaint.js"></script>
   <script src="js/NoteItem/NoteItemDisease.js"></script>
   <script src="js/NoteItem/NoteItemMedicalCheck.js"></script>
   <script src="js/NoteItem/NoteItemPrescription.js"></script>
+  <script src="js/NoteItem/NoteItemTreatment.js"></script>
   <script src="js/NoteItem/NoteItemOperation.js"></script>
   <script src="js/NoteItem/NoteItemScheme.js"></script>
   <script src="js/NoteItem/NoteItemMemo.js"></script>
@@ -255,6 +271,7 @@ $('button#OrcaModifyMedicalInfo').click(function(){
   <script src="js/NoteItemContainer/NoteItemContainerComplaint.js"></script>
   <script src="js/NoteItemContainer/NoteItemContainerDisease.js"></script>
   <script src="js/NoteItemContainer/NoteItemContainerMedicalCheck.js"></script>
+  <script src="js/NoteItemContainer/NoteItemContainerTreatment.js"></script>
   <script src="js/NoteItemContainer/NoteItemContainerPrescription.js"></script>
   <script src="js/NoteItemContainer/NoteItemContainerOperation.js"></script>
   <script src="js/NoteItemContainer/NoteItemContainerScheme.js"></script>
