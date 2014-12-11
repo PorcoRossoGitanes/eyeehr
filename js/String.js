fa6f6321f6c11feeaf44a836fc714b195bff1ed8
shokokb <shokokb@kobayashishouko-no-MacBook-Pro.local>
2014-12-11 10:14:37 +0900

    （１）String.format関数を追加した。

---
+++ js/String.js
@@ -0,0 +1,19 @@
+/**
+ * フォーマット関数
+ */
+String.prototype.format = function(arg) {
+    var rep_fn = undefined;
+
+    if (typeof arg == "object") {
+        rep_fn = function(m, k) {
+            return arg[k];
+        }
+    } else {
+        var args = arguments;
+        rep_fn = function(m, k) {
+            return args[parseInt(k)];
+        }
+    }
+
+    return this.replace(/\{(\w+)\}/g, rep_fn);
+}
